2218159683e0171e72bd7971f3613503e90c2385
Uemerson A. Santana <uemerson@icloud.com>
2021-09-01 22:52:22 -0300

    Initial Commit

---
+++ Dashboard Tab View With Swipe Gestures/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dashboard Tab View With Swipe Gestures
+//
+//  Created by Uemerson A. Santana on 01/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dashboard Tab View With Swipe Gestures/Dashboard_Tab_View_With_Swipe_GesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dashboard_Tab_View_With_Swipe_GesturesApp.swift
+//  Dashboard Tab View With Swipe Gestures
+//
+//  Created by Uemerson A. Santana on 01/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Dashboard_Tab_View_With_Swipe_GesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
